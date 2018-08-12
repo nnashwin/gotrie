b3d5086d48c779a6c2872898a4f1003e7046e41e
Tyler Boright <lupin3.ken@gmail.com>
2018-08-12 14:32:56 -0700

    changes the tn function to use a variable tn instead of t; adds a NewTrie function that initializes a Trie with the proper params and a unit test for the function

--- main.go
+++ main.go
@@ -14,17 +14,27 @@ type TrieNode struct {
 	Next *TrieNode
 }
 
-func (t *TrieNode) insertChar(s string) (err error) {
-	if t.Char != "" {
+func NewTrie() *Trie {
+	t := Trie{}
+	t.Root = &TrieNode{Char: "", Next: nil}
+
+	return &t
+}
+
+func (tn *TrieNode) insertChar(s string) (err error) {
+	if tn.Char != "" {
 		err = errors.New("The TrieNode already has a character, can not be inserted twice!")
 		return
 	}
 
-	t.Char = s
+	tn.Char = s
 
 	return
 }
 
 func main() {
 	fmt.Println("vim-go")
+	t := Trie{}
+	fmt.Println(t.Root == nil)
+	fmt.Println(&t == nil)
 }
--- main_test.go
+++ main_test.go
@@ -13,3 +13,12 @@ func TestInsertChar(t *testing.T) {
 		t.Error("The insertChar method did not successfully insert the character to the node")
 	}
 }
+
+func TestNewTrie(t *testing.T) {
+	expected := &Trie{Root: &TrieNode{Char: "", Next: nil}}
+	actual := NewTrie()
+
+	if reflect.DeepEqual(expected, actual) == false {
+		t.Error("The NewTrie function did not create a correct trie")
+	}
+}
