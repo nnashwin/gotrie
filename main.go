7e4772900ba00d95e602e78630298e545718eb55
Tyler Boright <lupin3.ken@gmail.com>
2018-08-12 17:17:46 -0700

    adds the AddString method to the library to add a String to an instantiated Trie object; removes the insertChar function because it isn't needed any longer

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"errors"
 	"fmt"
+	"strings"
 )
 
 type Trie struct {
@@ -10,13 +11,13 @@ type Trie struct {
 }
 
 type TrieNode struct {
-	Char string
-	Next *TrieNode
+	Char     string
+	Children map[string]*TrieNode
 }
 
 func NewTrie() *Trie {
 	t := Trie{}
-	t.Root = &TrieNode{Char: "", Next: nil}
+	t.Root = &TrieNode{Char: "", Children: make(map[string]*TrieNode)}
 
 	return &t
 }
@@ -30,6 +31,24 @@ func (tn *TrieNode) insertChar(s string) (err error) {
 	tn.Char = s
 
 	return
+}
+
+func (t *Trie) AddString(s string) (err error) {
+	if t.Root == nil {
+		err = errors.New("The Trie was not initialized with the proper params, please either use the NewTrie method or initialize the trie with a correct Root and trie again")
+	}
+
+	currentNode := *t.Root
+	ss := strings.Split(s, "")
+	for _, ch := range ss {
+		if _, ok := currentNode.Children[ch]; ok == false {
+			currentNode.Children[ch] = &TrieNode{Char: ch, Children: make(map[string]*TrieNode)}
+		}
+
+		currentNode = *currentNode.Children[ch]
+	}
+
+	return nil
 }
 
 func main() {
--- main_test.go
+++ main_test.go
@@ -5,20 +5,28 @@ import (
 	"testing"
 )
 
-func TestInsertChar(t *testing.T) {
-	expected := &TrieNode{Char: "t"}
-	actual := TrieNode{}
-	actual.insertChar("t")
-	if reflect.DeepEqual(actual.Char, expected.Char) == false {
-		t.Error("The insertChar method did not successfully insert the character to the node")
-	}
-}
-
 func TestNewTrie(t *testing.T) {
-	expected := &Trie{Root: &TrieNode{Char: "", Next: nil}}
+	expected := &Trie{Root: &TrieNode{Char: "", Children: make(map[string]*TrieNode)}}
 	actual := NewTrie()
 
 	if reflect.DeepEqual(expected, actual) == false {
 		t.Error("The NewTrie function did not create a correct trie")
+	}
+}
+
+func TestAddString(t *testing.T) {
+	tr := NewTrie()
+	ss := "tyler"
+
+	tr.AddString(ss)
+
+	node := tr.Root
+	for _, ch := range ss {
+		// Convert to string to use correctly in the Children map
+		strCh := string(ch)
+		if _, ok := node.Children[strCh]; ok == false {
+			t.Errorf("The AddString function did not write the correct string to the trie. The Children map should contains %s and did not", strCh)
+		}
+		node = (node.Children[strCh])
 	}
 }
